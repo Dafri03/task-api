0520ce61d29f4be27e3b72bcf6b3ecf9765d325d
Dylan Afrifa-Akwamoah <k.afrifa03@gmail.com>
2026-04-14 23:53:27 -0400

    document completed query parameter on GET /tasks

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,12 +1,25 @@
 import * as taskService from '../services/taskService.js';
 
 export async function getTasks(req, res, next) {
-  const tasks = await taskService.getAllTasks();
-  res.json(tasks);
+  try {
+    let completed = undefined;
+    if (req.query.completed !== undefined) {
+      completed = req.query.completed === 'true';
+    }
+
+    const tasks = await taskService.getAllTasks(completed);
+    res.json(tasks);
+  } catch (err) {
+    next(err);
+  }
 }
 
 export async function createTask(req, res, next) {
-  const { title, completed } = req.body;
-  const task = await taskService.createTask({ title, completed });
-  res.status(201).json(task);
+  try {
+    const { title, completed } = req.body;
+    const task = await taskService.createTask({ title, completed });
+    res.status(201).json(task);
+  } catch (err) {
+    next(err);
+  }
 }
